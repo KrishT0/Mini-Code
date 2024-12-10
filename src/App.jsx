6f40cc99e274b2c3ab0e75e88c62cf6c08ef1d1b
KrishT0 <krshnabiswal619@gmail.com>
2024-12-10 16:08:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderComponent from "@/components/custom/header";
+
+function App() {
+  return (
+    <div className="border-2 max-w-[1200px] ">
+      <HeaderComponent />
+    </div>
+  );
+}
+
+export default App;
