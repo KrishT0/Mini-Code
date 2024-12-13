38377b6b64761108fbb667b6ad103cf9903ab58e
KrishT0 <krshnabiswal619@gmail.com>
2024-12-14 03:32:31 +0530

    fixed require not found on deployment issue

--- src/components/custom/header.jsx
+++ src/components/custom/header.jsx
@@ -6,7 +6,7 @@ function HeaderComponent() {
     <div className="max-w-[1200px] h-[10%] mx-auto py-5 relative">
       <h1 className="text-center text-2xl font-semibold">Mini Code</h1>
       <div className="absolute right-1 top-3">
-        {/* <ModeToggle /> */}
+        <ModeToggle />
       </div>
     </div>
   );
