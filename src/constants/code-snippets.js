cbe6ba9c191e7bb8754a698c9ad58cd81b99ebb0
KrishT0 <krshnabiswal619@gmail.com>
2024-12-17 12:35:10 +0530

    added go lang support and removed junk code

--- src/constants/code-snippets.js
+++ src/constants/code-snippets.js
@@ -6,4 +6,5 @@ export const codeSnippets = {
   csharp: `\nusing System;\n\npublic class HelloWorld\n{\n\tpublic static void Main(string[] args)\n\t{\n\t\tConsole.WriteLine("Hello world");\n\t}\n}\n`,
   php: '<?php\n\necho "Hello world";\n',
   cpp: `\n#include <iostream>\n\nint main() {\n  std::cout << "Hello world" << std::endl;\n  return 0;\n}\n`,
+  go: `\npackage main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello world")\n}\n`,
 };
--- src/constants/language-versions.js
+++ src/constants/language-versions.js
@@ -1,6 +1,7 @@
 export const languageList = {
   cpp: "10.2.0",
   csharp: "6.12.0",
+  go: "1.16.2",
   java: "15.0.2",
   javascript: "18.15.0",
   php: "8.2.3",
