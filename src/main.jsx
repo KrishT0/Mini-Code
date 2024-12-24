6c908594d4826075093ba681b3c84ac91fa42ec9
KrishT0 <krshnabiswal619@gmail.com>
2024-12-25 00:08:04 +0530

    added analytics and insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import App from "@/App.jsx";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -10,6 +12,8 @@ createRoot(document.getElementById("root")).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <QueryClientProvider client={queryClient}>
       <App />
+      <SpeedInsights />
+      <Analytics />
     </QueryClientProvider>
   </ThemeProvider>
 );
