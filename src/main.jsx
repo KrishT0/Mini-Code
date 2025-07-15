a061204d07675b8fadcb3995428172243100606f
Krishna Biswal <krshnabiswal619@gmail.com>
2025-07-15 21:12:20 +0530

    removed analytcs and insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import App from "@/App.jsx";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -12,8 +10,6 @@ createRoot(document.getElementById("root")).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <QueryClientProvider client={queryClient}>
       <App />
-      <SpeedInsights />
-      <Analytics />
     </QueryClientProvider>
   </ThemeProvider>
 );
